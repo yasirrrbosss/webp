56d896ad8ef47b7b4168e036b78b9df6144783af
yasirrrbosss <157872779+yasirrrbosss@users.noreply.github.com>
2024-02-02 10:07:32 +0700

    Delete assets directory

--- assets/js/splitype-helper.js
+++
@@ -1,1 +0,0 @@
-{const t=window.SplitType,{parseDataAttr:a}=dataAttrHelpers,e={string:["tagName","lineClass","wordClass","charClass","splitClass","types","split"],number:[],boolean:["absolute"]},s=s=>{if(!(s.cloneNode(!0)instanceof Element))return;const l=a(s.getAttribute("data-split-text")||"",e);new t(s,{...l})};dataAttrHelpers.watchDataAttr("data-split-text",s)}
